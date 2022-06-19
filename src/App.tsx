8ab441b165e7ff093125c1f6ab44ab3595c2ed06
Alexey <44631357+agmitron@users.noreply.github.com>
2022-06-20 03:26:38 +0700

    refactor: pitch detection

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { ACF2PLUS, YIN, Macleod, AMDF } from "pitchfinder";
 import { Maqsoum } from "./data/compositions";
 import "./App.css";
 import Tact from "./components/Tact";
+
 import { bpmToMilliseconds, millisecondsToBPM } from "./utils/tempo.utils";
+import { isFrequencyCorrect } from "./utils/frequency.utils";
+
+const detectPitch = AMDF();
 
 function App() {
   const [composition] = useState(Maqsoum);
@@ -10,6 +15,11 @@ function App() {
   const [currentFraction, setCurrentFraction] = useState(0);
   const [currentTact, setCurrentTact] = useState(0);
   const [intervalId, setIntervalId] = useState<number | null>(null);
+  const [currentFrequency, setCurrentFrequency] = useState(0);
+
+  const requestRef = useRef<number | null>(null);
+  const analyserNodeRef = useRef<AnalyserNode | null>(null);
+  const bufferRef = useRef<Float32Array | null>(null);
 
   const isPlaying = Boolean(intervalId);
 
@@ -27,23 +37,70 @@ function App() {
     setTempo(bpmToMilliseconds(+bpm));
   };
 
+  const stop = () => {
+    if (intervalId) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+      disablePitcher();
+    }
+  };
+
+  const pitch = () => {
+    if (bufferRef.current && analyserNodeRef.current) {
+      analyserNodeRef.current.getFloatTimeDomainData(bufferRef.current);
+      const frequency = detectPitch(bufferRef.current);
+      frequency && setCurrentFrequency(frequency);
+      requestRef.current = requestAnimationFrame(pitch);
+    }
+  };
+
+  const enablePitcher = async () => {
+    const audioContext = new window.AudioContext();
+    const micStream = await window.navigator.mediaDevices.getUserMedia({
+      audio: true,
+    });
+
+    const analyserAudioNode = audioContext.createAnalyser();
+    analyserAudioNode.fftSize = 2048;
+    // analyserAudioNode.minDecibels = -100;
+    // analyserAudioNode.maxDecibels = -10;
+    // analyserAudioNode.smoothingTimeConstant = 0.3;
+
+    const sourceAudioNode = audioContext.createMediaStreamSource(micStream);
+    sourceAudioNode.connect(analyserAudioNode);
+    const buffer = new Float32Array(analyserAudioNode.fftSize);
+
+    analyserNodeRef.current = analyserAudioNode;
+    bufferRef.current = buffer;
+    requestRef.current = requestAnimationFrame(pitch);
+  };
+
+  const disablePitcher = () => {
+    if (requestRef.current) {
+      cancelAnimationFrame(requestRef.current);
+      requestRef.current = null;
+    }
+  };
+
   const play = () => {
     const interval = window.setInterval(() => {
       setCurrentFraction((prev) => prev + 1);
     }, tempo);
 
     setIntervalId(interval);
-  };
-
-  const stop = () => {
-    if (intervalId) {
-      clearInterval(intervalId);
-      setIntervalId(null);
-    }
+    enablePitcher();
   };
 
   useEffect(() => {
+    return () => {
+      disablePitcher();
+    };
+  }, []);
+
+  useEffect(() => {
     if (!intervalId) {
+      setCurrentTact(0);
+      setCurrentFraction(0);
       return console.log("Finished.");
     }
 
@@ -56,10 +113,16 @@ function App() {
       setCurrentFraction(0);
       return setCurrentTact((prev) => prev + 1);
     }
+
+    if (isFrequencyCorrect(expectedFrequency, currentFrequency)) {
+      console.log(isFrequencyCorrect(expectedFrequency, currentFrequency));
+    }
   }, [
+    composition.pattern,
     composition.pattern.length,
     composition.size,
     currentFraction,
+    // currentFrequency,
     currentTact,
     intervalId,
   ]);
@@ -73,11 +136,21 @@ function App() {
     };
   }, [intervalId]);
 
+
+  const expectedFrequency =
+    composition.pattern?.[currentTact]?.[currentFraction]?.unit?.frequency ?? 0;
+
   return (
     <main>
       <section className="composition">
         <header className="composition__header">
           <h1 className="composition__title">{composition.name}</h1>
+          <p className="composition__frequency">
+            Expected: {expectedFrequency.toFixed(2)} Hz
+          </p>
+          <p className="composition__frequency">
+            Received: {currentFrequency.toFixed(2)} Hz
+          </p>
           <button onClick={isPlaying ? stop : play}>
             {isPlaying ? "Stop" : "Play"}
           </button>
---
+++ src/utils/frequency.utils.ts
@@ -0,0 +1,3 @@
+export const isFrequencyCorrect = (expected: number, received: number) => {
+    return received <= expected + 5 && received >= expected - 5;
+};

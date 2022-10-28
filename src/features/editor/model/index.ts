c509fe9f86f7f591eab70dd9ed45e1902a6479e5
Alexey <44631357+agmitron@users.noreply.github.com>
2022-10-28 20:03:12 +0800

    refactor: parsing konnakol

--- src/features/editor/model/index.ts
+++ src/features/editor/model/index.ts
@@ -1,77 +1,9 @@
 import { combine, createStore, sample } from 'effector';
-import { Pattern } from '~/entities/composition/model';
-import Tact from '~/entities/composition/model/Tact';
-import Chord from '~/entities/unit/model/Chord';
-import Note, { isNote } from '~/entities/unit/model/Note';
-import Roll from '~/entities/unit/model/Roll';
 import { compositionNameChanged, editCompositionNameButtonClicked, konnakolChanged, saveCompositionNameButtonClicked } from '../ui';
-import { $unitsAsMapping, ShorcutsToUnits } from './toolbar';
+import parseKonnakol from './parser';
+import { $unitsAsMapping } from './toolbar';
 
-function parseKonnakol(unitsShortcutsMapping: ShorcutsToUnits, konnakol: string): Pattern {
-  if (!konnakol) {
-    return [];
-  }
 
-  return konnakol
-    .split('\n')
-    .map(line => new Tact(
-      line
-        .trim()
-        .split(' ')
-        .map(symbol => {
-          // TODO: move it somewhere else
-          const unit = unitsShortcutsMapping[symbol];
-          const isChord = /^\((\S+\|?)+\)$/.test(symbol)
-          const isRoll = /^\[\S+,?\]$/.test(symbol)
-
-          if (isChord) {
-            const chordSymbols = symbol.replaceAll(/\(|\)/g, '').split('|')
-            return new Chord(
-              chordSymbols.map(symbol =>
-                unitsShortcutsMapping[symbol] ?? new Note({ frequencies: [], symbol, color: 'grey' })
-              )
-            )
-          }
-
-          if (isRoll) {
-            const rollSymbols = symbol.replaceAll(/\[|\]/g, '').split(',')
-            return new Roll(
-              rollSymbols.map(symbol => {
-                const unit = unitsShortcutsMapping[symbol]
-                const isChord = /^\((\S+\|?)+\)$/.test(symbol)
-
-                if (isChord) {
-                  const chordSymbols = symbol.replaceAll(/\(|\)/g, '').split('|')
-                  return new Chord(
-                    chordSymbols.map(symbol => {
-                      const unit = unitsShortcutsMapping[symbol]
-
-                      if (unit) {
-                        return unit
-                      }
-
-                      return new Note({ frequencies: [], symbol, color: 'grey' })
-                    })
-                  )
-                }
-
-                if (isNote(unit)) {
-                  return unit
-                }
-
-                return new Note({ frequencies: [], symbol, color: 'grey' })
-              })
-            )
-          }
-
-          if (unit) {
-            return unit;
-          }
-
-          return new Note({ frequencies: [], symbol, color: 'grey' })
-        })
-    ))
-}
 
 export const $compositionName = createStore('');
 export const $konnakol = createStore('');
---
+++ src/features/editor/model/parser.ts
@@ -0,0 +1,61 @@
+import { Pattern } from '~/entities/composition/model';
+import Tact from '~/entities/composition/model/Tact';
+import Chord from '~/entities/unit/model/Chord';
+import Note, { isNote } from '~/entities/unit/model/Note';
+import Roll from '~/entities/unit/model/Roll';
+import Unit from '~/entities/unit/model/Unit';
+import { ShorcutsToUnits } from './toolbar';
+
+const isChordSymbol = (symbol: string) => /^\((\S+\|?)+\)$/.test(symbol)
+const isRollSymbol = (symbol: string) => /^\[\S+,?\]$/.test(symbol)
+
+const getChordNotes = (chordSymbol: string) => chordSymbol.replaceAll(/\(|\)/g, '').split('|')
+const getRollUnits = (rollSymbol: string) => rollSymbol.replaceAll(/\[|\]/g, '').split(',')
+
+export default function parseKonnakol(
+  unitsShortcutsMapping: ShorcutsToUnits,
+  konnakol: string
+): Pattern {
+  if (!konnakol) {
+    return [];
+  }
+
+  const parseNote = (symbol: string) => {
+    const unit = unitsShortcutsMapping[symbol];
+
+    if (unit && isNote(unit)) {
+      return unit;
+    }
+
+    return new Note({ frequencies: [], symbol, color: 'grey' })
+  }
+
+  const parseUnit = (symbol: string): Unit => {
+    if (isChordSymbol(symbol)) {
+      const notesSymbols = getChordNotes(symbol)
+      return new Chord(notesSymbols.map(parseNote))
+    }
+
+    if (isRollSymbol(symbol)) {
+      const unitsSymbols = getRollUnits(symbol);
+      return new Roll(unitsSymbols.map(s => {
+        if (isChordSymbol(s)) {
+          return new Chord(getChordNotes(s).map(parseNote))
+        }
+
+        return parseNote(s)
+      }))
+    }
+
+    return parseNote(symbol)
+  }
+
+  return konnakol
+    .split('\n')
+    .map(line => new Tact(
+      line
+        .trim()
+        .split(' ')
+        .map(parseUnit)
+    ))
+}
